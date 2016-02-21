922874e34f7f8f84010f3a612b7c4e0d80a610f6
hienbx94 <hienbx94@gmail.com>
2016-02-21 22:58:27 +0700

    add apid

--- app/helpers/markers_helper.rb
+++ app/helpers/markers_helper.rb
@@ -3,6 +3,6 @@ require 'ffi'
 module MarkersHelper
   extend FFI::Library
   ffi_lib 'c'
-  ffi_lib 'public/libgenTexData.so'
+  ffi_lib 'libgenTexData.so'
   attach_function :genTexData, [:string], :void
 end
