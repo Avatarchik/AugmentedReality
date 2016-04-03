68c7dfb1aa1ad679827c4086c26b018d908ba679
hienbx94 <hienbx94@gmail.com>
2016-04-03 19:33:17 +0700

    fix header

--- config/initializers/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:#{Rails.application.secrets.redis_port}/0" }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:#{Rails.application.secrets.redis_port}/0" }
-end
