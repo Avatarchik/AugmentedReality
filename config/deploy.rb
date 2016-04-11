8a6464daea64cc5cb8bf224c8e5b2c24bfa5727c
hienbx94 <hienbx94@gmail.com>
2016-04-11 15:43:05 +0700

    add sidekiq

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
+set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
 
 ## Defaults:
 # set :scm,           :git
