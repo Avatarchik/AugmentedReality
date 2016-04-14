cd77f552b4b74e4624eb7540c93fd003d5e4bd0a
hienbx94 <hienbx94@gmail.com>
2016-04-14 21:51:04 +0700

    add sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:#{Rails.application.secrets.redis_port}/0" }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:#{Rails.application.secrets.redis_port}/0" }
-end
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:#{Rails.application.secrets.redis_port}/0" }
+# end
+#
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: "redis://#{Rails.application.secrets.redis_host}:#{Rails.application.secrets.redis_port}/0" }
+# end
