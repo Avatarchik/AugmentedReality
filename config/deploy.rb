9670bba3e6228204583b6c67ad1abbe20d1f2b05
hienbx94 <hienbx94@gmail.com>
2016-05-06 20:54:57 +0700

    fix errors

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,8 +76,8 @@ namespace :deploy do
     end
   end
 
-  desc 'Runs rake db:dbreset'
-  task :dbreset => [:set_rails_env] do
+  desc 'Runs rake db:databasereset'
+  task :databasereset => [:set_rails_env] do
     on primary fetch(:migration_role) do
       within release_path do
         with rails_env: fetch(:rails_env) do
